ed8448951b8b9c9883e1a67d3549488c4cd94ea2
Roberto <hooverlds93@gmail.com>
2022-12-13 09:56:31 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route} from 'react-router-dom';
+
+import Header from './pages/Header'
+import Inicio from './pages/Inicio';
+import Acerca from './pages/Acerca';
+import Contacto from './pages/Contacto'
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/acerca' element={<Acerca />} />
+        <Route path='/contacto' element={<Contacto />} />
+      </Routes>
     </div>
+
   );
 }
 
---
+++ src/pages/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <NavLink className="navbar-brand" to="/">Navbar</NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" aria-current="page" to="/">Inicio</NavLink >
+                        </li>
+                        <li class="nav-item">
+                            <NavLink className="nav-link" to="/acerca">Acerca</NavLink >
+                        </li>
+                        <li class="nav-item">
+                            <NavLink className="nav-link" to="/contacto">Contacto</NavLink >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
